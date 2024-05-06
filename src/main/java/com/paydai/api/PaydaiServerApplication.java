f8de11a43c25efc2e00dd3050d550bb0977f5578
DanielAdek <maildaniel.me1@gmail.com>
2024-05-06 18:13:58 +0100

    setup

---
+++ src/main/java/com/paydai/api/PaydaiServerApplication.java
@@ -0,0 +1,13 @@
+package com.paydai.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaydaiServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaydaiServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/paydai/api/PaydaiServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paydai.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaydaiServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
