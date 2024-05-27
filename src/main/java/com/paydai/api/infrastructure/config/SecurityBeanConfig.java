dc636d55dff0a071e9364d2d0e62e58fd2c61fd6
DanielAdek <maildaniel.me1@gmail.com>
2024-05-27 10:03:21 +0100

    use cors

--- src/main/java/com/paydai/api/infrastructure/config/SecurityBeanConfig.java
+++ src/main/java/com/paydai/api/infrastructure/config/SecurityBeanConfig.java
@@ -1,16 +1,8 @@
 package com.paydai.api.infrastructure.config;
 
-import com.paydai.api.domain.repository.AccountRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SecurityBeanConfig {
-  private final AccountRepository repository;
-
-  @Autowired
-  public SecurityBeanConfig(AccountRepository repository) {
-    this.repository = repository;
-  }
 
 }
--- src/main/java/com/paydai/api/infrastructure/config/WebSecurityBeanConfig.java
+++ src/main/java/com/paydai/api/infrastructure/config/WebSecurityBeanConfig.java
@@ -1,6 +1,5 @@
 package com.paydai.api.infrastructure.config;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,18 +7,22 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 
 @Configuration
 @EnableWebSecurity
-@RequiredArgsConstructor
 public class WebSecurityBeanConfig {
-//  private final JwtAuthFilter jwtAuthFilter;
 
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
     return httpSecurity
       .csrf(AbstractHttpConfigurer::disable)
+      .cors(cors -> cors.configurationSource(corsConfigurationSource()))
       .authorizeHttpRequests(
         req -> req.requestMatchers(
           "api/v1/**",
@@ -30,7 +33,23 @@ public class WebSecurityBeanConfig {
           .anyRequest()
           .authenticated())
       .sessionManagement(ses -> ses.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-//      .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
       .build();
   }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        configuration.setAllowedOrigins(Arrays.asList("*")); // Allow all origins
+
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS")); // Allow all methods
+
+        configuration.setAllowedHeaders(Arrays.asList("*")); // Allow all headers
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
+    }
 }
